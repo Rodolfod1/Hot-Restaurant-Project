4e0d26a3c4c133c252f1c45188e145beb52c9383
Enxhi Salaj <enxhi_salaj@yahoo.com>
2020-11-17 19:56:57 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Dependencies
-const e = require("express");
 var express = require("express");
 var path = require("path");
 
@@ -11,13 +10,52 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Tables Data
+var tables = [
+    {
+        costumerName: "rodo",
+        phoneNumber: "455546",
+        customerEmail: "rod@diaz",
+        customerID: "55656"
+    },
+    {
+        customerName: "Ryan Stout",
+        phoneNumber: "5868542383",
+        customerEmail: "rstout039@gmail.com",
+        customerID: "43462frhg66"
+    }
+]
+
 // ROUTES
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "table.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "viewTables.html"));
 });
+
+app.get("/api/tables", function(){
+    res.sendFile(path.join(__dirname, ".html"));
+});
+
+app.get("/api/tables/:table", function(req, res){
+    var choice = req.params.table;
+
+    for(var i = 0; i < 5; i++){
+        if(choice === tables[i].routeName){
+            return res.json(tables[i]);
+        }
+        return res.json(false);
+    }
+});
+
+// create POST
+app.post("/api/tables", function(req, res)) {
+    var newReservation = req.body;
+    newReservation.routeName = newReservation.costumerName.replace(/\s+/g, "").toLowerCase();
+    tables.push(newReservation);
+    res.json(newReservation);
+}
 
 // start the server
 
-app.listen(PORT, function(){
-    console.log("App listening on PORT "+ PORT);
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 })
